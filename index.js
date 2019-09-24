41db9eea216b4195e2a617bcd043acf3c56f1aab
Maxim Golman <sankeman2004@gmail.com>
2019-09-24 12:41:08 +0300

    force eslint check each commit. add temp rules so non-bugs will not be errors for now

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ module.exports = {
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended"
     ],
-    // "rules": {
-    //     "@typescript-eslint"
-    // }
+    "rules": {
+        // all should be errors
+        "@typescript-eslint/no-namespace": 1,
+        "@typescript-eslint/no-use-before-define": 1,
+        "@typescript-eslint/camelcase": 1,
+        "@typescript-eslint/no-var-requires": 1,
+        "no-inner-declarations": 1,
+        "require-atomic-updates": 1,
+        // all should be errors
+    }
 }
