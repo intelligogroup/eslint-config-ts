edec92a83d07812577f3cebdb5f5baf94b70b780
mp-intelligo <mpechthalt@intelligo.ai>
2019-10-27 15:56:42 +0200

    to work with typescript

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "plugins": ["@typescript-eslint", "eslint-plugin-node"],
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:node/recommended"
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint"
     ],
-    "rules": {
+    parserOptions: {
+        sourceType: "module"
+    },
+    rules: {
         // all should be errors
         "@typescript-eslint/no-namespace": 1,
         "@typescript-eslint/no-use-before-define": 1,
         "@typescript-eslint/camelcase": 1,
         "@typescript-eslint/no-var-requires": 1,
         "no-inner-declarations": 1,
-        "require-atomic-updates": 1,
-        "node/no-extraneous-require": ["error", {
-            "allowModules": ['database', 'infra', 'scheduler', 'nlp', 'utility-adapter', 'infra_api', 'server-health']
-        }],
+        "require-atomic-updates": 1
         // all should be errors
     }
 }
